d8735f626c6584a80f001854077dd47ab2ce2ad6
nikita-skodin <skodin36@gmail.com>
2024-03-05 13:49:26 +0300

    Initial commit

---
+++ src/main/java/com/nikita/taskcreateserviceforcv/TaskCreateServiceForCvApplication.java
@@ -0,0 +1,13 @@
+package com.nikita.taskcreateserviceforcv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskCreateServiceForCvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskCreateServiceForCvApplication.class, args);
+    }
+
+}
